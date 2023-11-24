cf286a9d17cb3e0480fe5fb3d13a0c5fdeacddd9
jermaine_jay <jsonosita@gmail.com>
2023-11-24 22:39:38 +0100

    changes.

--- Ecommerce.Services/Configurations/Jwt/JwtConfig.cs
+++ Ecommerce.Services/Configurations/Jwt/JwtConfig.cs
@@ -3,7 +3,7 @@
     public class JwtConfig
     {
         public string? Secret { get; set; }
-        public double? Expires { get; set; }
+        public double Expires { get; set; }
         public string? ImpersonationExpires { get; set; }
         public string? Issuer { get; set; }
         public string? Audience { get; set; }
