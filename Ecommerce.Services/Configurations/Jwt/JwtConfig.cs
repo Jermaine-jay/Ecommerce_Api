8e0618c97aa2628452790baff11fedbd6aa4d4d8
jermaine_jay <jsonosita@gmail.com>
2023-11-24 22:36:27 +0100

    made some classes nullable.

--- Ecommerce.Services/Configurations/Jwt/JwtConfig.cs
+++ Ecommerce.Services/Configurations/Jwt/JwtConfig.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ecommerce.Services.Configurations.Jwt
+﻿namespace Ecommerce.Services.Configurations.Jwt
 {
     public class JwtConfig
     {
-        public string Secret { get; set; }
-        public double Expires { get; set; }
-        public string ImpersonationExpires { get; set; }
-        public string Issuer { get; set; }
-        public string Audience { get; set; }
+        public string? Secret { get; set; }
+        public double? Expires { get; set; }
+        public string? ImpersonationExpires { get; set; }
+        public string? Issuer { get; set; }
+        public string? Audience { get; set; }
     }
 }
--- Ecommerce.Services/Infrastructure/Settings.cs
+++ Ecommerce.Services/Infrastructure/Settings.cs
@@ -1,14 +1,12 @@
-﻿
-
-using Ecommerce.Services.Configurations.Cache.CacheServices;
-using Ecommerce.Services.Configurations.Jwt;
+﻿using Ecommerce.Services.Configurations.Cache.CacheServices;
 using Ecommerce.Services.Configurations.Email;
+using Ecommerce.Services.Configurations.Jwt;
 
 namespace Ecommerce.Services.Infrastructure
 {
     public class Settings
     {
-        public Authentication? Authentication { get; set; } 
+        public Authentication? Authentication { get; set; }
         public JwtConfig? JwtConfig { get; set; }
         public RedisConfig? redisConfig { get; set; }
         public ZeroBounceConfig? ZeroBounceConfig { get; set; }
@@ -25,22 +23,22 @@ namespace Ecommerce.Services.Infrastructure
 
     public class Google
     {
-        public string ClientSecret { get; set; }
-        public string ClientId { get; set; }
+        public string? ClientSecret { get; set; }
+        public string? ClientId { get; set; }
     }
 
     public class Facebook
     {
-        public string AppId { get; set; }
-        public string AppSecret { get; set; }
+        public string? AppId { get; set; }
+        public string? AppSecret { get; set; }
     }
 
 
     public class CloudinarySettings
     {
-        public string CloudName { get; set; }
-        public string ApiKey { get; set; }
-        public string ApiSecret { get; set; }
+        public string? CloudName { get; set; }
+        public string? ApiKey { get; set; }
+        public string? ApiSecret { get; set; }
     }
 
 }
