0886a8338db0ce9dbd4f07201e4366536686a9cc
Johnson Osita <80931376+Germaine-jay@users.noreply.github.com>
2023-11-30 18:13:52 +0100

    seeded roles and role claims.

--- Ecommerce_Api/Program.cs
+++ Ecommerce_Api/Program.cs
@@ -102,9 +102,9 @@ app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
 
 app.MapControllers();
 
-/*await app.SeedRole();
+await app.SeedRole();
 await app.ClaimSeeder();
-await app.ProductSeeder();
+/*await app.ProductSeeder();
 await app.SeededUserAsync();*/
 
 app.Run();
